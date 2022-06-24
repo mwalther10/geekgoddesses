9becbea4481441565f47ec0344d9846a117bc6bd
Marina Walther <marina.walther@gmail.com>
2022-06-24 13:46:08 +0200

    add icons and logo

--- shopping-bud/src/lib/header/Header.svelte
+++ shopping-bud/src/lib/header/Header.svelte
@@ -4,13 +4,13 @@
 
 <header>
 
-	<nav class="flex items-center justify-center h-24 bg-light-grey  text-xl xl:text-2xl w-full font-bold">
-		<picture>
-			<img class="w-24 h-24" src="logo2.svg" alt="rocket_cart_logo" />
+	<nav class="flex items-center justify-between h-24 bg-light-grey  text-xl xl:text-2xl w-full font-bold">
+		<picture class="flex items-center w-20">
+			<img src="logo4.svg" alt="logo" />
 		</picture>
-		<ul class="flex items-center justify-center w-fit">
-			<li class={$page.url.pathname === '/' ? "text-yellow" : "text-white" + " mx-4"}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class={$page.url.pathname === '/about' ? "text-yellow": "text-white" + " mx-4"}>
+		<ul class="flex items-center justify-between w-40 mx-4">
+			<li class={ $page.url.pathname === '/' ? "text-red" : "text-white"}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class={ $page.url.pathname === '/about' ? "text-red": "text-white"}>
 				<a sveltekit:prefetch href="/about">About</a>
 			</li>
 		</ul>
---
+++ shopping-bud/src/lib/icon.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let data = ''
+    export let viewBox = '0 0 256 256' // default value
+    
+    export let size; // h-10 h-12 h-32 h-64 h-96 h-120 h-6 h-4 h-8|w-10 w-12 w-32 w-64 w-96 w-120 w-6 w-4 w-8
+    export let color;
+    export let padding; // p-1 p-2 p-3 p-4 p-5 p-6 p-7 p-8
+    export let additionalStyle = ""; 
+    $: classdata = "stroke-" + color + " fill-" + color + " w-" + size + " h-" + size + " p-" + padding + " " + additionalStyle
+    $: elements = data
+      .replace(/<svg ([^>]*)>/, '')
+      .replace('</svg>', '')
+/* stroke-dark-500 fill-dark-500 stroke-blue-500 fill-blue-500 fill-white stroke-white fill-main stroke-main fill-dark stroke-dark fill-warning stroke-warning fill-secondary stroke-secondary* */
+</script>
+  
+<svg
+xmlns="http://www.w3.org/2000/svg"
+{viewBox}
+class={classdata}
+{...$$restProps}
+>
+{@html elements}
+</svg>
