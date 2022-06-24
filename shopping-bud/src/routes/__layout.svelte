a0a29d551a8b460f586a6e27364773bfe1d27aa1
Marina Walther <marina.walther@gmail.com>
2022-06-24 09:42:34 +0200

    clean up repository + added first logo idea

--- shopping-bud/src/routes/__layout.svelte
+++ shopping-bud/src/routes/__layout.svelte
@@ -3,43 +3,16 @@
 	import '../app.css';
 </script>
 
-<Header />
 
-<main>
-	<slot />
-</main>
+<div class="flex flex-col justify-between h-screen">
+	<Header />
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+	<main class="bg-white mb-auto flex grow ">
+		<slot />
+	</main>
+	
+	<footer class="invisible">
+		<p>ShoppingBud!</p>
+	</footer>
+</div>
 
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
--- shopping-bud/tailwind.config.cjs
+++ shopping-bud/tailwind.config.cjs
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "red": "#E4572E",
+        "yellow": "#FFC914",
+        "blue": "#17BEBB",
+        "green": "#76B041",
+        "black": "#1E2433",
+        "dark": "#2C3763",
+        "white": "#FEFFFE",
+        "dark-grey": '#596475',
+        "light-grey": "#DCE0E5"
+      },
+    }
   },
   plugins: []
 };
