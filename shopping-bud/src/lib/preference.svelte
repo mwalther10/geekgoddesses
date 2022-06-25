ac9eb347350462a5ba364b37dde24c51f93e59b6
Shideh Almasian <almasian@stud.uni-heidelberg.de>
2022-06-25 13:29:59 +0200

    filters and preferences

---
+++ shopping-bud/src/lib/preference.svelte
@@ -0,0 +1,54 @@
+<script>
+	import Icon from './icon.svelte';
+	export let icon;
+	export let title;
+	export let options;
+
+	
+	// $:disabled=check_disable(options);
+	// $:console.log(disabled)
+
+	// function check_disable(opts){
+	// 	opts.forEach(element => {
+	// 	if (element.value!=-1){
+	// 		return false;
+	// 	}
+	// });
+	// return true;
+	// }
+    /* range-accent range-secondary range-primary * */
+
+</script>
+
+
+<div class="collapse">
+	<input type="checkbox" /> 
+	<div class="collapse-title text-l font-medium">
+		<div class="flex flex-row " style="border-radius: 0px;">
+			<Icon data={icon} color={'dark'} size="8" padding="1"  />
+			<div class="text-l mr-2 pt-1">{title}</div>
+			{#each options as option, i}
+			{#if option.value!=-1}
+			<div class="badge text-sm badge-{option.color} mr-2">{option.value}{option.unit}</div>
+			{/if}
+			{/each}
+            <!-- {#if disabled}
+            {disabled}
+            <div class="badge text-sm badge-accent mr-2">disabled</div>
+            {/if} -->
+		</div>
+	</div>
+	<div class="collapse-content"> 
+			{#each options as option, i}
+			<div class="form-control flex justify-center items-center flex-none">
+				  <span class="label-text p-2">{option.label} - ({option.value} {option.unit})</span>
+                  <input type="range" min="{option.min}" max="{option.max}" bind:value={option.value} step="{option.steps}"  class="range range-{option.color}" />
+			  </div>
+			{/each}
+			</div>
+
+  </div>
+<div class="flex flex-col items-center justify-center w-full">
+	
+	
+</div>
