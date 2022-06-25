273dc409a88222be13048e5de390190aeff0735c
Marina Walther <marina.walther@gmail.com>
2022-06-25 16:41:00 +0200

    added bullet list

--- shopping-bud/src/routes/shopping-list.svelte
+++ shopping-bud/src/routes/shopping-list.svelte
@@ -3,21 +3,22 @@
 </script>
 
 <script>
-	import Location from "../lib/location.svelte";
-	let selectedStore = "Kaufland Heidelberg-Weststadt";
-	
+	import BulletList from '$lib/bullet-list.svelte';
+	import { location } from '../stores/location';
+	import Location from '../lib/location.svelte';
+	import { shoppingList } from '../stores/shopping-list-store';
 </script>
 
 <svelte:head>
 	<title>Shopping List</title>
 	<meta name="description" content="ShoppingBud" />
 </svelte:head>
-<div data-theme="cupcake" class="flex flex-col font-medium font-nunito ">
-
+<div data-theme="cupcake" class="flex flex-col font-medium font-nunito w-full ">
 	<Location />
-
-	
-
-
-
+	{#if $location != ''}
+		<BulletList />
+	{/if}
+	{#if $shoppingList.length > 0}
+		<div>TEst</div>
+	{/if}
 </div>
